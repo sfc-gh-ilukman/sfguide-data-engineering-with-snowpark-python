e966e71a3ec9e6c66de463b8f058c3452e45c3a6
Irfaan Lukman <irfaan.lukman@snowflake.com>
2025-10-22 02:25:23 +0000

    Changes v4

--- steps/05_fahrenheit_to_celsius_udf/fahrenheit_to_celsius_udf/function.py
+++ steps/05_fahrenheit_to_celsius_udf/fahrenheit_to_celsius_udf/function.py
@@ -16,6 +16,7 @@ def main(temp_f: float) -> float:
     return convert_temperature(float(temp_f), 'F', 'C')
 
 
+
 # For local debugging
 # Be aware you may need to type-convert arguments if you add input parameters
 if __name__ == '__main__':
